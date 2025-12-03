e91f0404c910bdac7854498957017ec6e027fb80
codeleaded <codeleaded@gmail.com>
2025-12-03 22:40:58 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,22 +1,34 @@
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/FluidField.h"
+
+#define FLUID_INTERACTION_RADIUS 10.0f
+
+FluidField field;
 
 void Setup(AlxWindow* w){
-    Clear(BLACK);
+    field = FluidField_New(
+        GetWidth(w),
+        GetHeight(w)
+    );
 }
 void Update(AlxWindow* w){
     if(Stroke(ALX_MOUSE_L).DOWN){
-        
+        const Vec2 dir = Vec2_Mulf(Vec2_Sub(GetMouse(),GetMouseBefore()),10.0f);
+        FluidField_Interact(&field,GetMouse().x,GetMouse().y,dir,FLUID_INTERACTION_RADIUS);
     }
+
+    FluidField_Update(&field,w->ElapsedTime);
     
     Clear(BLACK);
-    RenderCStr("Hello World",10,10,BLUE);
+    
+    FluidField_Render(WINDOW_STD_ARGS,&field,(Vec2){ 0.0f,0.0f });
 }
 void Delete(AlxWindow* w){
-    
+    FluidField_Free(&field);
 }
 
 int main() {
-    if(Create("Fluid Sim - Flow Field",1000,1000,1,1,Setup,Update,Delete)){
+    if(Create("Fluid Sim - Flow Field",125,75,16,16,Setup,Update,Delete)){
         Start();
     }
     return 0;
